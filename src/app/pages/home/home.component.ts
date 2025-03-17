4f20c7eaae5dd8a3c644a1b020b9b8e7c69ea95d
AndreGomes <67878678+AndreLuisGomes@users.noreply.github.com>
2025-03-16 21:07:16 -0300

    ultimos arquivos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { QuizzComponent } from "../../components/quizz/quizz.component";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [QuizzComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
